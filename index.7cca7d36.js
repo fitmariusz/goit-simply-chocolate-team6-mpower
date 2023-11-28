80a96288dabb6dde7161ef3538941d129877f271
fitmariusz <72906237+fitmariusz@users.noreply.github.com>
2023-11-28 23:19:22 +0000

    Deploying to gh-pages from @ fitmariusz/goit-simply-chocolate-team6-mpower@a3fe092d69280adf82dbe71800904f6eaba56b45 🚀

--- index.7cca7d36.js
+++
@@ -1,3 +0,0 @@
-var e=new Splide("#slider1",{perPage:4,focus:0,arrows:!1,wheel:!0,breakpoints:{1199:{perPage:2.5},767:{perPage:1}}});e.mount();var e=new Splide("#slider2",{perPage:3,focus:0,arrows:!1,wheel:!0,breakpoints:{1199:{perPage:2},767:{perPage:1}}});e.mount();//# sourceMappingURL=index.7cca7d36.js.map
-
-//# sourceMappingURL=index.7cca7d36.js.map
